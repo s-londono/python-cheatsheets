6a2f2a9c73a67482b72b2b8108f672f23005ec35
slondono <s.londono.zapata@gmail.com>
2019-12-28 19:27:31 -0500

    DataFrames.

--- pandas-basics.py
+++ pandas-basics.py
@@ -41,7 +41,53 @@ print(se_lbl_ix + se_lbl_ix_5)
 # Ensures repeatability of results
 np.random.seed(101)
 
+# A DataFrame is a bunch of Series sharing indexes (each column is a Series, each row is also a series)
 # Create a DataFrame specifying the row and column indexes
 df_1 = pd.DataFrame(data=randn(5, 4), index=['A', 'B', 'C', 'D', 'E'], columns=['W', 'X', 'Y', 'Z'])
+
+# Retrieve column by label (as a Series)
+se_col_x = df_1['X']
+print(f"Type: {type(se_col_x)}. As string: \n{se_col_x}")
+
+# Add column to a DataFrame, as a sum of existing columns
+df_1['SumXY'] = df_1['X'] + df_1['Y']
+
+# Remove a row (axis by default is 0). Does not modify the DataFrame
+df_1.drop('E')
+
+# Remove a column. Does not modify the DataFrame
+df_1.drop('Z', axis=1)
+
+# To modify the DataFrame use inplace (applies to many methods in Pandas)
+df_1.drop('Z', axis=1, inplace=True)
+
+# Dimensions of a DataFrame
+print(df_1.shape)
+
+# Select a subset of columns
+print(df_1[['Y', 'W']])
+
+# Select rows by label: use loc (location. Access a group of rows and columns by label(s) or a boolean array)
+se_row_a = df_1.loc['A']
+print(f"Type: {type(se_row_a)}. As string:\n{se_row_a}")
+print(df_1.loc[['A', 'C']])
+
+# With loc, the first argument specifies rows, second argument columns. Can use label slices, label arrays
+print(df_1.loc['A', 'X'])
+print(df_1.loc['B':'D', ['X', 'W']])
+print(df_1.loc['B':'D', 'X':'Y'])
+print(df_1.loc[['B', 'E'], 'X':'Y'])
+print(df_1.loc[['B', 'E'], 'X':'Y'])
+print(df_1.loc[['B', 'E'], 'X':'Y'])
+
+# With loc, boolean arrays can be used to select rows
+print(df_1.loc[df_1['X'] > 0])
+print(df_1.loc[df_1['X'] > 0, 'Y':])
+
+# Select rows by index: use iloc (index location)
+print(df_1.iloc[3])
+
+# With iloc, the first and second arguments specify row and column indexes respectively
+print(df_1.iloc[2, 3])
 
 
