563d7768f627c1130fc09549d82b8cf393648651
slondono <santiago@b2chat.io>
2020-02-10 21:35:48 -0500

    Completed documentation of package.

--- data_science/lessons/software_engineering/modularized_code/setup.py
+++ data_science/lessons/software_engineering/modularized_code/setup.py
@@ -1,3 +1,10 @@
+# Install a package in PIP, locally
+# - Add a setup.py file beside the package folder (distributions in this case)
+# - Run pip install . from the directory containing setup.py.
+# - Now can use the package from anywhere!
+#   E.g. from distributions import Gaussian
+# - Check the package location with:
+#   distributions.__file__
 from setuptools import setup
 
 setup(name="distributions", version="0.1", description="Probability distributions", packages=["distributions"], zip_safe=False)
