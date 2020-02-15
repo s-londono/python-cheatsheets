7db512a9aac349c157110d36b058e08b3026369d
slondono <santiago@b2chat.io>
2020-02-15 11:44:12 -0500

    documentation: Fixed error in documentation
    In the __init__.py file of the distributions module, fixed some documentation lines that were not prefixed with #
    
    #lesson_6

--- data_science/lessons/software_engineering/modularized_code/4a_binomial_package/distributions/__init__.py
+++ data_science/lessons/software_engineering/modularized_code/4a_binomial_package/distributions/__init__.py
@@ -45,27 +45,27 @@ from .GaussianDistribution import Gaussian
 
 # UPLOADING TO TEST PYPI
 
-- Create package dist/tar:
-python setup.py sdist
-
-- Install Twine:
-pip install twine
-
-- Upload package to the Test Pypi repo (update version in setup.py if necessary):
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-
-- Check the package exists in your testpypi account
-
-- Test installing the package from Test Pypi:
-pip install --index-url https://test.pypi.org/simple/ {package_name}
-UPLOADING TO PYPY
-
-twine upload dist/*
-
-- Uninstall previously installed from Test Pypi:
-pip uninstall {package_name}
-
-- Reinstall from Pypi:
-pip install {package_name}
-
-* NOTE: Dashes (-) should be replaced with underscores (_) when using the package in Python code
+# - Create package dist/tar:
+# python setup.py sdist
+#
+# - Install Twine:
+# pip install twine
+#
+# - Upload package to the Test Pypi repo (update version in setup.py if necessary):
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+#
+# - Check the package exists in your testpypi account
+#
+# - Test installing the package from Test Pypi:
+# pip install --index-url https://test.pypi.org/simple/ {package_name}
+# UPLOADING TO PYPY
+#
+# twine upload dist/*
+#
+# - Uninstall previously installed from Test Pypi:
+# pip uninstall {package_name}
+#
+# - Reinstall from Pypi:
+# pip install {package_name}
+#
+# * NOTE: Dashes (-) should be replaced with underscores (_) when using the package in Python code
