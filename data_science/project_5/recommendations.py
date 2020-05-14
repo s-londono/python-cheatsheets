2fcf7cdb37a8665d609207d9b28b0c3a20688213
slondono <s.londono.zapata@gmail.com>
2020-05-13 19:54:12 -0500

    feature: Project 5 - Recommendations
    Started project 5

--- data_science/project_5/recommendations.py
+++ data_science/project_5/recommendations.py
@@ -17,10 +17,29 @@ del df_content['Unnamed: 0']
 df_head = df.head()
 df_content_head = df_content.head()
 
+# PART I: EXPLORATORY DATA ANALYSIS
+
+# 1. What is the distribution of how many articles a user interacts with in the dataset? Provide visual and
+# descriptive statistics to assist with giving a look at the number of times each user interacts with an article
+
 # How many articles each user interacts with in the dataset
 df_article_x_user = df[["article_id", "email"]]
 df_count_artices_x_user = df_article_x_user.groupby(by=["email"]).count()
+df_count_artices_x_user.columns = ["count_articles"]
 
-df_count_artices_x_user.plot(kind="hist")
-plt.title("Total articles read by user")
+# Bunch of descriptive statistics
+descript_stats = df_count_artices_x_user.describe(percentiles=[0.10, 0.25, 0.50, 0.75, 0.85, 0.95, 0.99])
 
+df_count_artices_x_user["count_category"] = \
+    df_count_artices_x_user["count_articles"].apply(lambda c: c if c < 40 else 45)
+
+df_count_articles_low = df_count_artices_x_user[df_count_artices_x_user["count_articles"] < 40]
+
+# Plot histogram
+plt.hist(x=df_count_artices_x_user["count_category"], bins=20)
+plt.title("Total articles read per user")
+plt.show()
+
+plt.boxplot(x=df_count_articles_low["count_articles"])
+plt.title("Total articles read per user")
+plt.show()
